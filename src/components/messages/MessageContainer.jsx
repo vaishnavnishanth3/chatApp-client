536d9215ff9858cca6aed074ae4bfbc2b019657d
Vaishnav Nishanth AV <vaishnavnishanth3@gmail.com>
2024-07-22 16:55:25 +0530

    Socket and message shake animation added

--- src/components/messages/MessageContainer.jsx
+++ src/components/messages/MessageContainer.jsx
@@ -5,6 +5,7 @@ import MessageInput from "./MessageInput"
 import { TiMessages } from "react-icons/ti";
 import useConversation from "../../zustand/useConversation";
 import { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
 
 function MessageContainer() {
   const {selectedConversation, setSelectedConversation} = useConversation()
@@ -29,10 +30,11 @@ function MessageContainer() {
 }
 
 function NoChatSelected() {
-    return (
+  const {authUser} = useAuthContext();  
+    return ( 
       <div className="flex items-center justify-center w-full h-full">
           <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-              <p>Welcome John Doe</p>
+              <p>Welcome 👋 {authUser.fullName}</p>
               <p>Select a Chat to Start messaging </p>
               <TiMessages className='text-3xl md:text-6xl text-center'/>  
           </div>
--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,10 +1,19 @@
-// import React from 'react'
+import { useEffect, useRef } from "react";
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
-
+import useListenMessages from "../../hooks/useListenMessages";
 function Messages() {
   const {messages, loading} = useGetMessages()
+  useListenMessages()
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoview({ behavior: "smooth"});
+    }, 100)
+  }, [messages]);
+
   return (
     <div className="px-4 flex-1 overflow-auto">
         {!loading && messages.length > 0 && messages.map((message) => (<Message key={message._id} message={message}/>))}
